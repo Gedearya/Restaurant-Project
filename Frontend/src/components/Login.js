4089b73d9e1b99f8a8b96f4a411091b57ec5f24a
gedearya <igd.ad.pratama@gmail.com>
2023-04-30 07:00:57 +0700

    update project frontend

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -106,19 +106,6 @@ class Login extends Component {
                       value="Login"
                     />
                     <hr className="my-4" />
-                    <button
-                      className="btn btn-lg btn-google btn-block text-uppercase"
-                      type="submit"
-                    >
-                      <i className="fab fa-google mr-2" /> Sign in with Google
-                    </button>
-                    <button
-                      className="btn btn-lg btn-facebook btn-block text-uppercase"
-                      type="submit"
-                    >
-                      <i className="fab fa-facebook-f mr-2" /> Sign in with
-                      Facebook
-                    </button>
                     <div className="custom-control mt-3">
                       <Link to="/register">Create an Account!</Link>
                     </div>
